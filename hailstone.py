0695f2cdb8e4193eed77c138761244237aa6437a
CalebChen768 <72332844+CalebChen768@users.noreply.github.com>
2025-02-06 15:58:53 +0100

    hailstone algorithm + regex

---
+++ hailstone.py
@@ -0,0 +1,46 @@
+import time
+import string
+import threading
+
+class HailStone:
+    def __init__(self, machine_id, start_time=1738851309614):
+        """
+            a simple version of snowflake algorithm, and convert to base62
+            args:
+                machine_id: int, the machine id
+                start_time: int, the start time of the algorithm
+        """
+        self.machine_id = machine_id
+        self.start_time = start_time
+        self.sequence = 0
+        self.last_timestamp = -1
+
+        self._base62_alphabet = string.digits + string.ascii_letters
+
+    def fakesnow(self):
+        # it is a simple version of snowflake algorithm
+
+        time_now = int(time.time() * 1000) - self.start_time
+
+        # incremental sequence number in a single millisecond
+        if(time_now == self.last_timestamp):
+            self.sequence += 1
+        else:
+            self.sequence = 0
+            self.last_timestamp = time_now
+        
+        # 41 bits for time, 10 bits for machine id, 5 bits for sequence
+        return (time_now << 15) | (self.machine_id << 5) | self.sequence
+
+    def _base62(self, num):
+        # encode the number to base62
+        if num == 0:
+            return self._base62_alphabet[0]
+        base62 = []
+        while num:
+            num, rem = divmod(num, 62)
+            base62.append(self._base62_alphabet[rem])
+        return ''.join(reversed(base62))
+
+    def generate(self):
+        return self._base62(self.fakesnow())
---
+++ restAPI.py
@@ -0,0 +1,73 @@
+from flask import Flask, request, jsonify, Response
+import base64
+import time
+from hailstone import HailStone
+import re
+app = Flask(__name__)
+url_dict = {}
+
+@app.route('/<id>', methods=['GET'])
+def getURL(id):
+    if id in url_dict:
+        return jsonify({"value": url_dict[id]}), 301
+    else:
+        return jsonify({"error": "ID not found"}), 404
+
+@app.route('/<id>', methods=['PUT'])
+def updateURL(id):
+    url = request.get_json(force=True)['url']
+    
+    if id in url_dict:
+        if not checkURLValidity(url):
+            return jsonify({"error": "Invalid URL"}), 400
+        url_dict[id] = url
+        return jsonify({"message": "Updated successfully"}), 200
+    elif id not in url_dict:
+        return jsonify({"error": "ID not found"}), 404
+
+@app.route('/<id>', methods=['DELETE'])
+def deleteURL(id):
+    if id in url_dict:
+        url_dict.pop(id)
+        return Response(status=204)
+    else:
+        return jsonify({"error": "ID not found"}), 404
+
+@app.route('/', methods=['GET'])
+def getURLs():
+    return jsonify({"keys": list(url_dict.keys())}), 200
+
+@app.route('/', methods=['POST'])
+def putURL():
+    url = request.get_json()['value']
+    if url is None or url == "" or not checkURLValidity(url):
+        return jsonify({"error": "Invalid URL"}), 400
+    else:
+        id = shortenURL(url)
+        url_dict[id] = url
+        return jsonify({"id": id}), 201
+
+@app.route('/', methods=['DELETE'])
+def deleteAll():
+    url_dict.clear()
+    return Response(status=404)
+
+
+def shortenURL(url):
+    # url += str(time.time())
+    # id = base64.b64encode(url.encode()).decode()
+    # return id[-6:]
+    hs = HailStone(1)
+    return hs.generate()
+
+def checkURLValidity(url):
+    # urlRegx = r"(http|https)://[a-zA-Z0-9\./]+"
+    urlRegx = r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
+    # https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+    return re.match(urlRegx, url)
+
+
+if __name__ == '__main__':
+    app.run(port=8000)
+    
+    
